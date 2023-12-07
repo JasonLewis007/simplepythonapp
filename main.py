1e162773e26f22fc46e5c2f96c3ded0773908d1f
Jason Lewis <jlewis@harperlove.com>
2023-12-07 16:23:41 -0500

    4th commit

--- main.py
+++ main.py
@@ -2,21 +2,35 @@ import tkinter as tk
 
 
 def on_button_click():
-    label.config(text="Hello, GUI App!")
+    """Function triggered when the button is clicked."""
+    label.config(text="Button clicked! Welcome to my GUI App!")
 
 
-# Create the main window
+# Initialize the main window
 app = tk.Tk()
-app.title("My GUI App")
+app.title("My Amazing GUI App")
 
-#Create a button
-button = tk.Button(app, text="Click me!", command=on_button_click)
-button.pack(pady=10)
+# Create a button with a descriptive command text and bind it to the function
+button = tk.Button(
+    app,
+    text="Click me to see magic!",
+    command=on_button_click,
+    # Add padding for better visual appeal
+    padx=10,
+    pady=10,
+)
 
-# Create a label
-label = tk.Label(app, text="Welcome to my GUI App!")
-label.pack(pady=10 )
+# Pack the button in the window
+button.pack()
+
+# Create and configure the label with initial text
+label = tk.Label(app, text="Click the button to get started!", font=("Arial", 12))
+
+# Pack the label with padding
+label.pack(pady=10)
 
 # Start the main event loop
-app.mainloop
+app.mainloop()
+
+
 
