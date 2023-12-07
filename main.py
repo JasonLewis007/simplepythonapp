5f86245fef6c39fdacd7d0ded50bd3d628e6fe3b
Jason Lewis <jlewis@harperlove.com>
2023-12-07 15:36:08 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def on_button_click():
+    label.config(text="Hello, GUI App!")
+
+
+# Create the main window
+app = tk.Tk()
+app.title("My GUI App")
+
+#Create a button
+butto = tk.Button(app, text="Click me!", command=on_button_click)
+butto.pack(pady=10)
+
+# Create a label
+label = tk.Label(app, text="Welcome to my GUI App!")
+label.pack(pady=10 )
+
+# Start the main event loop
+app.mainloop
+
